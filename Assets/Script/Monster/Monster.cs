6455ab82a3c9151a0dbe5c3b94de8fa7791691ac
Patrick <pwcarney@bu.edu>
2021-04-01 23:48:30 -0400

    Added nav mesh and monster

---
+++ Assets/Script/Monster/Monster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster : MonoBehaviour
+{
+    private PlayerMovement player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+
+        FindObjectOfType<NavMeshSurface>().BuildNavMesh();
+    }
+
+    private void Update()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(player.transform.position);
+    }
+}
