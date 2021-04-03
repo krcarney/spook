e67b0f01a8271e64fb47702d8f5d6b534fcec65e
Patrick <pwcarney@bu.edu>
2021-04-03 17:55:26 -0400

    Update Player.cs

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -4,10 +4,5 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private void Awake()
-    {
-        // Place player
-        Spawn[] spawns = FindObjectsOfType<Spawn>();
-        transform.position = spawns[Random.Range(0, spawns.Length)].transform.position;
-    }
+
 }
